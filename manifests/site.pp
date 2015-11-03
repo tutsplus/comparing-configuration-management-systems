92b834341301021ea45fbf769eefe0054e237c6d
Markus Mühlberger <markus@mmuehlberger.com>
2015-11-03 13:23:41 +0100

    Add PG_HBA rule to database server

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,6 +8,14 @@ node default {
 
 node "database.localdomain" {
   class { 'tp-rails-app::database': }
+
+  postgresql::server:pg_hba_rule { 'allow subnet access to postgresql':
+    type => 'host',
+    database => 'rails_production',
+    user => 'rails',
+    address => '192.168.246.0/24',
+    auth_method => 'md5',
+  }
 }
 
 node "web.localdomain" {
