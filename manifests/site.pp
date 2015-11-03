77688fa2d8090897842a1562bbd08a255a94b196
Markus Mühlberger <markus@mmuehlberger.com>
2015-11-03 13:15:15 +0100

    Prepare for using multiple nodes

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,7 @@
 node default {
   class { 'tp-cron-puppet': }
-  class { 'tp-rails-app::database': }
-  class { 'tp-rails-app::web':
-    name => 'tutsplus',
-  }
+  # class { 'tp-rails-app::database': }
+  # class { 'tp-rails-app::web':
+  #   name => 'tutsplus',
+  # }
 }
