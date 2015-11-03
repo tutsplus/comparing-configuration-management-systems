7f7eb70b991aa208385b8d1fc34a51dd01f435e4
Markus Mühlberger <markus@mmuehlberger.com>
2015-11-03 13:37:49 +0100

    Listen to all addresses with postgresql

--- modules/tp-rails-app/manifests/database.pp
+++ modules/tp-rails-app/manifests/database.pp
@@ -2,6 +2,7 @@ class tp-rails-app::database {
   class { 'postgresql::server': }
 
   postgresql::server::db { 'rails_production':
+    listen_addresses => '0.0.0.0',
     user => 'rails',
     password => postgresql_password('rails', 'password'),
   }
