eb3b13ad74fdbff9081fb6a56fc69dad63f091ac
Markus Mühlberger <markus@mmuehlberger.com>
2015-11-03 11:58:34 +0100

    Add cron puppet module

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,3 @@
 node default {
-  class { 'nginx': }
-
-  nginx::resource::vhost { 'default':
-    listen_options => 'default',
-    www_root => '/usr/share/nginx/html',
-  }
+  class { 'tp-cron-puppet': }
 }
